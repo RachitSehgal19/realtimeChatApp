568b80b9a00f30a9c3f0e8fd24c9f22e2d17df4a
Rachit Sehgal <rachit.sehgal19@gmail.com>
2026-07-03 21:09:15 +0530

    Add trust proxy for secure cookies

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ import messageRouter from "./routes/message.routes.js"
 import { app, server } from "./socket/socket.js"
 
 const port=process.env.PORT || 5000
-
+app.set("trust proxy", 1)
 
 app.use(cors({
     origin: process.env.CLIENT_URL || "http://localhost:5173",
@@ -21,8 +21,6 @@ app.use(cookieParser())
 app.use("/api/auth",authRouter)
 app.use("/api/user",userRouter)
 app.use("/api/message",messageRouter)
-
-
 
 server.listen(port,()=>{
     connectDb()
